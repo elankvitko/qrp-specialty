895d868b295b3e17181378839ff8429bd08e4149
Elan Kvitko <elan@idknyc.com>
2017-08-29 17:27:24 -0400

    temp change

--- app/assets/javascripts/quickrx.js
+++ app/assets/javascripts/quickrx.js
@@ -1,29 +1,29 @@
-// $(".custom-icons")
-//   .mouseover(function () {
-//       var classes = this.className.split( " " )
-//
-//       if ( classes.length === 2 && classes[ 1 ] === "first-child" ) {
-//         var address = "#" + this.id + " .icon";
-//         var src = $(address).attr("src").match(/[^\.]+/) + "over.png";
-//         $(address).attr("src", src);
-//       } else if ( classes.length === 2 && classes[ 1 ] === "white-impt-bg" ) {
-//         false
-//       } else if ( classes.length === 1 ) {
-//         var address = "#" + this.id + " .icon";
-//         var src = $(address).attr("src").match(/[^\.]+/) + "over.png";
-//         $(address).attr("src", src);
-//       };
-//   })
-//   .mouseout(function () {
-//       var classes = this.className.split( " " )
-//
-//       if ( classes.length === 2 && classes[ 1 ] === "first-child" ) {
-//         var address = "#" + this.id + " .icon";
-//         var src = $(address).attr("src").replace("over.png", ".png");
-//         $(address).attr("src", src).fadeIn(700);
-//       } else if ( classes.length === 1 ) {
-//         var address = "#" + this.id + " .icon";
-//         var src = $(address).attr("src").replace("over.png", ".png");
-//         $(address).attr("src", src).fadeIn(700);
-//       };
-//   });
+$(".custom-icons")
+  .mouseover(function () {
+      var classes = this.className.split( " " )
+
+      if ( classes.length === 2 && classes[ 1 ] === "first-child" ) {
+        var address = "#" + this.id + " .icon";
+        var src = $(address).attr("src").match(/[^\.]+/) + "over.png";
+        $(address).attr("src", src);
+      } else if ( classes.length === 2 && classes[ 1 ] === "white-impt-bg" ) {
+        false
+      } else if ( classes.length === 1 ) {
+        var address = "#" + this.id + " .icon";
+        var src = $(address).attr("src").match(/[^\.]+/) + "over.png";
+        $(address).attr("src", src);
+      };
+  })
+  .mouseout(function () {
+      var classes = this.className.split( " " )
+
+      if ( classes.length === 2 && classes[ 1 ] === "first-child" ) {
+        var address = "#" + this.id + " .icon";
+        var src = $(address).attr("src").replace("over.png", ".png");
+        $(address).attr("src", src).fadeIn(700);
+      } else if ( classes.length === 1 ) {
+        var address = "#" + this.id + " .icon";
+        var src = $(address).attr("src").replace("over.png", ".png");
+        $(address).attr("src", src).fadeIn(700);
+      };
+  });
--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,5 @@
 class AboutController < ApplicationController
   def index
+    @article = params[ :article ]
   end
 end
